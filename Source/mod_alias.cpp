fa7f9d9b864b45851c95a032d203037154faac90
s-touchstone <steven_touchstone@outlook.com>
2019-02-01 17:01:58 -0600

    Mod_Alias testing

--- Source/mod_alias.cpp
+++ Source/mod_alias.cpp
@@ -7,12 +7,16 @@ using namespace std;
 
 void mod_alias(){
 	int each=0,selection,selection2;
-	string line,commands[100],names[100],trigger="off",lines[100],exit="false",first="alais ",quote="\'",new_line;
+	string line,commands[100],names[100],trigger="off",lines[100],exit="false",first="alias ",quote="\'",new_line,old_line,temp_line;
 	char new_info[100];
-	fstream file;
-	file.open("test.txt");
+//	fstream file;
+	ifstream ifile("test.txt");
+	ofstream ofile("test.txt");
+//	file.open("test.txt");
+//	ifile.open();
 //	file.open("~/.bashrc");
-	while(getline(file,line)){
+//	while(getline(file,line)){
+	while(getline(ifile,line)){
 		if(line=="#Custom aliases"){
 			trigger="on";
 			continue;
@@ -23,12 +27,12 @@ void mod_alias(){
 			continue;
 		}//end if
 	}//end while
-	file.close();
+//	file.close();
 	for(int i=0;i<each;i++){
 		size_t pos=lines[i].find("=");
 		commands[i]=lines[i].substr(pos+2);
 		commands[i]=commands[i].erase(commands[i].size()-1);
-		names[i]=lines[i].substr(5,pos-5);
+		names[i]=lines[i].substr(6,pos-6);
 	}//end for
 	while(exit!="true"){
 		cout<<"1. Change name"<<endl<<"2. Edit the command"<<endl<<"#";
@@ -52,6 +56,8 @@ void mod_alias(){
 		cin.ignore();
 		cout<<"Enter new information"<<endl<<"#";
 		cin.getline(new_info,sizeof(new_info));
+		old_line=first+names[selection2-1]+"="+quote+commands[selection2-1]+quote;
+		cout<<"OLD: "<<old_line<<endl;
 		if(selection==1){
 			new_line=first+new_info+"="+quote+commands[selection2-1]+quote;
 		}//end if
@@ -60,10 +66,21 @@ void mod_alias(){
 		}//end else
 		exit="true";
 	}//endwhile
-//	file.open("test.txt",ios::app);
+//	file.open("test.txt");
+//	ifile.open();
+//	ofile.open();
 //	file.open("~/.bashrc",ios::app);
-//	file<<new_line<<endl;
-//	file.close();
-//	line.replace(line.find(deleteline),deleteline.length(),"");
+	while(ifile >> temp_line){
+		if(temp_line==old_line){
+//			cout<<"Found match: "<<line<<endl;
+//			line.replace(0,line.length(),new_line);
+			temp_line=old_line;
+		}//end if
+		temp_line+="\n";
+		ofile << temp_line;
+//		line.replace(line.find("alias "+names[selection2-1]),line.length()-1,new_line);
+	}//end while
+//	ifile.close();
+//	ofile.close();
 	cout<<"NEW: "<<new_line<<endl<<endl;
 }//end mod_alias
